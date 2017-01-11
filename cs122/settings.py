ff3749b4adfd433369f1270aaeb2bf07acdcc831
Christine <cszchung@gmail.com>
2017-01-11 17:18:30 -0600

    fixed static dir

--- cs122/settings.py
+++ cs122/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, '..', 'meethalfway', 'static')
 
 # Additional locations of static files
 STATICFILES_DIRS = []
